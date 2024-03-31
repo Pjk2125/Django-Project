5f2058c9363c9dac478a6d7c5801f998d73aaccf
Prathamesh Kulkarni <prathameshkulkarni2125@gmail.com>
2024-04-01 00:22:47 +0530

    Update settings.py

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_database_name',
-        'USER': 'your_database_user',
-        'PASSWORD': 'your_database_password',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample_data.mysql',
+        'USER': 'sample',
+        'PASSWORD': 'YES',
         'HOST': 'localhost',
         'PORT': '5432',
     }
