3c715db4ccb4c367812309126e775d5e2dab094c
Prathamesh Kulkarni <prathameshkulkarni2125@gmail.com>
2024-04-02 17:55:35 +0530

    Update tests.py

--- my_project/task_app/tests.py
+++ my_project/task_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import YourModel
+
+class YourModelAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.your_model = YourModel.objects.create(name='Test Model')
+
+    def test_get_your_model(self):
+        url = reverse('your-model-detail', kwargs={'pk': self.your_model.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
